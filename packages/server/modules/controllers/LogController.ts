7e5f476cf237beeb66e76a9027d99ac67c91909b
Felipe Bergamin <felipebergamin6@gmail.com>
2017-12-20 16:28:03 -0200

    corrigido método parseQuery no LogController

--- packages/server/modules/controllers/LogController.ts
+++ packages/server/modules/controllers/LogController.ts
@@ -14,26 +14,33 @@ export class LogController {
     const query = LogController.parseQuery(req.query);
 
     try {
-      res.json(await Log.find(query).sort({ dataHora: -1 }).exec());
+      res.json(await Log.find(query).populate("usuario").sort({ dataHora: -1 }).exec());
     } catch (err) {
       res.status(400).json(err);
     }
   }
 
-  private static parseQuery(queryString: any) {
-    const query = {} as any;
-
-    if ("texto" in queryString) {
-      query.$text = {
-        $search: queryString.texto,
-      };
+  private static parseQuery(query: any = {}) {
+    for (const property in query) {
+      if (query[property] === 'null' || query[property] == null || query[property] === '') {
+        delete query[property];
+      }
     }
 
-    if ("minDate" in queryString && "maxDate" in queryString) {
-      query.dataHora = {
-        $gte: queryString.minDate,
-        $lte: queryString.maxDate,
+    if ("texto" in query) {
+      query.$text = {
+        $search: query.texto,
       };
+      delete query.texto;
+    }
+
+    if ("minDate" in query && "maxDate" in query) {
+      query.dataHora = {
+        $gte: query.minDate,
+        $lte: query.maxDate,
+      };
+      delete query.minDate;
+      delete query.maxDate;
     }
 
     return query;
