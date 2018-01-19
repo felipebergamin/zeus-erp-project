47661f98ae0ceaa220baf81e1ee728ffe7bd0220
Felipe Bergamin <felipebergamin6@gmail.com>
2018-01-19 20:30:00 -0200

    atualizando pacote node-tl1-fiberhome para v1.0.0

--- packages/server/modules/services/FiberhomeService.ts
+++ packages/server/modules/services/FiberhomeService.ts
@@ -1,31 +1,41 @@
 import { TL1Client } from "node-tl1-fiberhome";
+
+import debug = require("../../debug");
 import { LogService as log } from "./LogService";
 
 let fhService: FiberhomeService;
 
 export class FiberhomeService {
-  public static instance(): FiberhomeService {
+
+  public static async instance(): Promise<FiberhomeService> {
     if (!fhService) {
-      fhService = new FiberhomeService();
+      try {
+        fhService = new FiberhomeService();
 
-      fhService.tl1 = new TL1Client("172.16.255.6", 3337, () => {
-        fhService.tl1.login("felipe", "fe.li.pe./28")
-          .then((login) => {
-            log.debug("conexão TL1 estabelecida com sucesso");
-            setInterval(() => {
-              fhService.tl1.handShake();
-            }, FiberhomeService.FIVE_MINUTES_IN_MS);
+        fhService.tl1 = new TL1Client("172.16.255.6", 3337);
+        await fhService.tl1.connect();
+        const loginResponse = await fhService.tl1.login("felipe", "fe.li.pe./28");
 
-            process.on('SIGINT', () => {
-              fhService.tl1.logout()
-                .then(fhService.tl1.end)
-                .catch(fhService.tl1.end);
-            });
-          })
-          .catch((err) => {
-            log.error(`falha no login do servidor TL1`, JSON.stringify(err));
+        if (loginResponse.parsedResponse.completion_code.includes("COMPLD")) {
+          debug("conexão TL1 estabelecida");
+          log.debug("conexão TL1 estabelecida");
+
+          setInterval(() => {
+            fhService.tl1.handShake();
+          }, FiberhomeService.FIVE_MINUTES_IN_MS);
+
+          process.on('SIGINT', () => {
+            debug("SIGINT: fechando conexão TL1");
+            log.debug("SIGINT: fechando conexão TL1");
+            fhService.tl1.logout()
+              .then(fhService.tl1.end)
+              .catch(fhService.tl1.end);
           });
-      });
+        }
+      } catch (err) {
+        log.error(`falha no login do servidor TL1`, JSON.stringify(err));
+        throw err;
+      }
     }
 
     return fhService;
