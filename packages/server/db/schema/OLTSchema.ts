4e920073ce0842f9c171a9c0d4f7d56af2016fef
Felipe Bergamin <felipebergamin6@gmail.com>
2018-01-29 15:11:56 -0200

    ajuste OltSchema

--- packages/server/db/schema/OLTSchema.ts
+++ packages/server/db/schema/OLTSchema.ts
@@ -5,13 +5,11 @@ export = new Schema({
     required: [true, "O IP da OLT deve ser informado"],
     type: String,
   },
-  login: String,
   nome: {
     required: [true, "Um nome é obrigatório"],
     type: String,
   },
   obs: String,
-  passwd: String,
 
   alteradoEm: require("../fields/alterado_em"),
   criadoEm: require("../fields/criado_em"),
