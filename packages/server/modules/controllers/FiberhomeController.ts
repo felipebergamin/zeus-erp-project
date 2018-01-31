009bc13b823d69089c7455277053a51676f34b86
Felipe Bergamin <felipebergamin6@gmail.com>
2018-01-31 11:45:36 -0200

    fiberhome controller agora espera receber o ObjectID da OLT, em vez do IP

--- packages/server/modules/controllers/FiberhomeController.ts
+++ packages/server/modules/controllers/FiberhomeController.ts
@@ -1,34 +1,42 @@
 import { Request, Response } from "express";
+
+import OLT = require("../../db/model/OLT");
 import { FiberhomeService } from "../services/FiberhomeService";
+import { handleError } from "../utils/HttpControllers";
 
 export class FiberhomeController {
   public static async getOnuInfo(req: Request, res: Response) {
     try {
       const { olt, slot, pon, onumac } = req.query;
 
-      const fh = FiberhomeService.instance();
-      const r = await fh.getOnuOpticalInfo(olt, slot, pon, onumac);
+      const oltObj = await OLT.findById(olt).exec();
+      const fh = await FiberhomeService.instance();
+      const r = await fh.getOnuOpticalInfo(oltObj.get("ip"), slot, pon, onumac);
+
       res.json(r.parsedResponse.values.pop());
     } catch (err) {
-      res.status(400).json(err);
+      handleError(err, res);
     }
   }
 
   public static async getUnauthorizedOnu(req: Request, res: Response) {
     try {
       const { olt } = req.query;
-      const fh = FiberhomeService.instance();
-      const r = await fh.getUnauthOnu(olt);
+
+      const oltObj = await OLT.findById(olt).exec();
+      const fh = await FiberhomeService.instance();
+      const r = await fh.getUnauthOnu(olt.ip);
+
       res.json(r.parsedResponse.values);
     } catch (err) {
-      res.status(400).json(err);
+      handleError(err, res);
     }
   }
 
   public static async addOnu(req: Request, res: Response) {
     try {
       const { NAME, OLTID, ONUID, ONUTYPE, SLOTNO, PONNO } = req.body;
-      const fh = FiberhomeService.instance();
+      const fh = await FiberhomeService.instance();
       const r = await fh.addOnu(NAME, OLTID, ONUID, ONUTYPE, SLOTNO, PONNO);
       res.json(r.parsedResponse);
     } catch (err) {
@@ -39,7 +47,7 @@ export class FiberhomeController {
   public static async configureOnu(req: Request, res: Response) {
     try {
       const { OLTID, ONUID, ONUPORT, PONID, PVID, VLANMOD } = req.body;
-      const fh = FiberhomeService.instance();
+      const fh = await FiberhomeService.instance();
       const r = await fh.configureOnu(OLTID, ONUID, ONUPORT, PONID, PVID, VLANMOD);
       res.json(r.parsedResponse);
     } catch (err) {
