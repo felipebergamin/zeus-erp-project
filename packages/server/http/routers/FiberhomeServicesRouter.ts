5a86146427c2c5c9d525de6d4e5937a467012b91
Felipe Bergamin <felipebergamin6@gmail.com>
2018-02-05 19:20:52 -0200

    validação para a rota /fiberhome/addonu

--- packages/server/http/routers/FiberhomeServicesRouter.ts
+++ packages/server/http/routers/FiberhomeServicesRouter.ts
@@ -3,6 +3,7 @@ import { Request, Response, Router } from "express";
 import { FiberhomeService } from "../../services/FiberhomeService";
 import { FiberhomeController } from "../controllers/FiberhomeController";
 
+import addOnuValidator = require("../validators/fiberhome/addOnu");
 import getOnuInfoValidator = require("../validators/fiberhome/getOnuInfo");
 import getUnauthOnuValidator = require("../validators/fiberhome/getUnauthOnu");
 
@@ -10,7 +11,7 @@ const router = Router();
 
 router.get("/onuinfo", getOnuInfoValidator, FiberhomeController.getOnuInfo);
 router.get("/getunauthonu", getUnauthOnuValidator, FiberhomeController.getUnauthorizedOnu);
-router.post("/addonu", FiberhomeController.addOnu);
+router.post("/addonu", addOnuValidator, FiberhomeController.addOnu);
 router.post("/configureonu", FiberhomeController.configureOnu);
 
 export = router;
