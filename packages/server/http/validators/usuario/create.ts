286db5ff2fbafa7627d54179c67c83fb7fd358c6
Felipe Bergamin <felipebergamin6@gmail.com>
2018-02-06 14:03:40 -0200

    mensagem de validação no validator de usuários

--- packages/server/http/validators/usuario/create.ts
+++ packages/server/http/validators/usuario/create.ts
@@ -10,26 +10,26 @@ export = [
 
   check("login")
     .exists().withMessage("O login deve ser informado")
-    .isAlpha(),
+    .isAlpha().withMessage("O login deve conter apenas letras"),
 
   check("nome")
     .exists().withMessage("O nome do usuário deve ser informado"),
 
   check("passwd")
-    .exists()
+    .exists().withMessage("Uma senha deve ser informada")
     .isLength({ min: 8 }).withMessage("A senha deve ter 8 caracteres ou mais"),
 
   check("perfil")
-    .exists()
-    .isMongoId(),
+    .exists().withMessage("O perfil do usuário deve ser informado")
+    .isMongoId().withMessage("O perfil do usuário parece ser inválido"),
 
   check("telegramID")
     .optional({ checkFalsy: true })
-    .isInt(),
+    .isInt().withMessage("Telegram ID inválido"),
 
   check("tipo")
-    .exists()
-    .isIn([ "tecnico", "atendente", "gerente", "outro" ]),
+    .exists().withMessage("O tipo de usuário ")
+    .isIn([ "tecnico", "atendente", "gerente", "outro" ]).withMessage("Valor inválido"),
 
   processValidationResult,
 ];
