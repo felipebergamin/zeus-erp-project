fc7a6fec26a69ae8779d37bafd06e2723fffb74d
Felipe Bergamin <felipebergamin6@gmail.com>
2018-02-06 11:21:56 -0200

    validação para post e put no perfil do usuário

--- packages/server/http/routers/PerfilUsuarioRouter.ts
+++ packages/server/http/routers/PerfilUsuarioRouter.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
 import { PerfilUsuarioController } from "../controllers/PerfilUsuarioController";
 
+import createPerfilUsuario = require("../validators/perfil-usuario/create");
+
 const router = Router();
 
 router.route('/')
   .get(PerfilUsuarioController.getAll)
-  .post(PerfilUsuarioController.create);
+  .post(createPerfilUsuario, PerfilUsuarioController.create);
 
 router.route('/:id')
   .get(PerfilUsuarioController.get)
-  .put(PerfilUsuarioController.update)
+  .put(createPerfilUsuario, PerfilUsuarioController.update)
   .delete(PerfilUsuarioController.delete);
 
 export = router;
