91f4331484bdcfcc8731ad5ea4facc8d032ca277
Felipe Bergamin <felipebergamin6@gmail.com>
2018-02-16 16:37:21 -0200

    carne controller agora usa objeto repository

--- packages/server/http/controllers/CarneController.ts
+++ packages/server/http/controllers/CarneController.ts
@@ -1,60 +1,22 @@
 import { Request, Response } from 'express';
 import moment = require('moment');
 
-import Boleto = require('../../db/model/BoletoBancario');
-import Carne = require('../../db/model/Carne');
-import Cliente = require('../../db/model/Cliente');
 import { LogService as log } from "../../services/LogService";
 import * as utils from "../utils/HttpControllers";
 
+import { RepositoryCarne } from '../../services/repository/repository-carne';
+
 export class CarneController {
+
+  constructor(
+    private repoCarne: RepositoryCarne,
+  ) {}
 
   public async create(req: Request, res: Response) {
     try {
-      const cliente = await Cliente.findById(req.body.cliente);
-
-      if (!cliente) {
-        throw new Error('Cliente não informado');
-      }
-
-      const carneCriado = await new Carne(req.body).save();
-
-      if ('parcelas' in req.body && 'valor' in req.body && 'primeiroVencimento' in req.body) {
-        const qtdParcelas = +req.body.parcelas;
-        const vencimentoInicial = moment(req.body.primeiroVencimento);
-
-        if (!vencimentoInicial.isValid()) {
-          throw new Error('A data de vencimento é inválida');
-        }
-
-        if (isNaN(qtdParcelas)) {
-          throw new Error('Quantidade de parcelas deve ser um número');
-        }
-
-        const boletosDoCarne: any[] = [];
-
-        for (let i = 0; i < qtdParcelas; i++) {
-          const dataVencimento = vencimentoInicial.clone().add(i, 'months');
-
-          if (dataVencimento.isoWeekday() === 7) { // se o dia de vencimento for um domingo
-            dataVencimento.add(1, 'days'); // muda o vencimento para a próxima segunda
-          }
-
-          const boleto = new Boleto({
-            carne: carneCriado.id,
-            cliente: req.body.cliente,
-            contaBancaria: cliente.get('contaBancaria'),
-            dataVencimento,
-            valorCobranca: req.body.valor,
-          });
-
-          boletosDoCarne.push(boleto);
-          await boleto.save();
-          log.info(`criou o boleto ${boleto.get("numeroBoleto")}, IP: ${req.ip}`, req.user._id, boleto.id);
-        }
-      }
-      res.json(carneCriado);
-      log.info(`criou o carne ${carneCriado.id}, IP: ${req.ip}`, req.user._id, carneCriado.id);
+      const carne = await this.repoCarne.create(req.body);
+      log.info(`criou o carne ${carne._id}, IP: ${req.ip}`, req.user._id, carne._id);
+      res.json(carne);
     } catch (err) {
       utils.handleError(err, res);
     }
@@ -62,9 +24,9 @@ export class CarneController {
 
   public async get(req: Request, res: Response) {
     try {
-      const query = Carne.findById(req.params.id);
-      utils.aplyGetRequestOptionsToQuery(req, query);
-      res.json(await query.exec());
+      const { fields, populate } = req.query;
+      const carne = await this.repoCarne.get(req.params.id, { fields, populate });
+      res.json(carne);
     } catch (err) {
       utils.handleError(err, res);
     }
@@ -72,8 +34,9 @@ export class CarneController {
 
   public async getAll(req: Request, res: Response) {
     try {
-      const query = utils.createQueryAndApplyReqOptions(req, Carne);
-      res.json(await query.exec());
+      const { fields, populate, ...search } = req.query;
+      const carnes = await this.repoCarne.getAll(search, { fields, populate });
+      res.json(carnes);
     } catch (err) {
       utils.handleError(err, res);
     }
@@ -81,30 +44,9 @@ export class CarneController {
 
   public async remove(req: Request, res: Response) {
     try {
-      const carne = await Carne.findById(req.params.id);
-
-      if (!carne) {
-        return res.status(404).json({ message: "Carnê não encontrado" });
-      }
-
-      const boletosDoCarne = await Boleto.where('carne', carne._id).exec();
-
-      boletosDoCarne.forEach((boleto: any) => {
-        boleto.set({
-          excluido: true,
-          excluidoEm: new Date(),
-        });
-        boleto.save();
-        log.info(`removeu o boleto ${boleto.get("numeroBoleto")}, IP: ${req.ip}`, req.user._id, boleto.id);
-      });
-
-      carne.set({
-        excluido: true,
-        excluidoEm: new Date(),
-      });
-      res.json(await carne.save());
-      log.info(`removeu o carnê ${carne.id}, IP: ${req.ip}`, req.user._id, carne.id);
-
+      const carneRemovido = await this.repoCarne.remove(req.params.id);
+      log.info(`removeu o carnê ${carneRemovido._id}, IP: ${req.ip}`, req.user._id, carneRemovido._id);
+      res.json(carneRemovido);
     } catch (err) {
       utils.handleError(err, res);
     }
--- packages/server/http/routers/CarneRouter.ts
+++ packages/server/http/routers/CarneRouter.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+
 import { CarneController } from '../controllers/CarneController';
+
+import { RepositoryBoleto } from '../../services/repository/repository-boleto';
+import { RepositoryCarne } from '../../services/repository/repository-carne';
+import { RepositoryCliente } from '../../services/repository/repository-cliente';
 
 import createCarneValidator = require("../validators/carne/create");
 
 const router = Router();
-const controller = new CarneController();
+
+const repoBoleto = new RepositoryBoleto();
+const repoCliente = new RepositoryCliente();
+const repoCarne = new RepositoryCarne(repoBoleto, repoCliente);
+
+const controller = new CarneController(repoCarne);
 
 router.route('/')
   .get(controller.getAll)
