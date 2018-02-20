e9137b5fd3307a3f8749ecf081d7bb0017da57f8
Felipe Bergamin <felipebergamin6@gmail.com>
2018-02-20 09:15:38 -0300

    registra models na inicialização do mongoose

--- packages/server/db/connection.ts
+++ packages/server/db/connection.ts
@@ -1,6 +1,19 @@
 import * as mongoose from 'mongoose';
 import debug = require('../debug');
 
+import boletoSchema = require("./schema/BoletoBancarioSchema");
+import carneSchema = require("./schema/CarneSchema");
+import chamadoTecnicoSchema = require("./schema/ChamadoTecnicoSchema");
+import clienteSchema = require("./schema/ClienteSchema");
+import contaBancariaSchema = require("./schema/ContaBancariaSchema");
+import instalacaoSchema = require("./schema/InstalacaoSchema");
+import ipPoolSchema = require("./schema/IPPoolSchema");
+import logSchema = require("./schema/LogSchema");
+import oltSchema = require("./schema/OLTSchema");
+import perfilUsuarioSchema = require("./schema/PerfilUsuarioSchema");
+import planoSchema = require("./schema/PlanoSchema");
+import usuarioSchema = require("./schema/UsuarioSchema");
+
 (mongoose as any).Promise = global.Promise;
 mongoose.connect('mongodb://localhost/zeus', { useMongoClient: true });
 
@@ -22,5 +35,18 @@ process.on('SIGINT', () => {
     process.exit(0);
   });
 });
+
+mongoose.model("BoletoBancario", boletoSchema);
+mongoose.model("Carne", carneSchema);
+mongoose.model("ChamadoTecnico", chamadoTecnicoSchema);
+mongoose.model("Cliente", clienteSchema);
+mongoose.model("ContaBancaria", contaBancariaSchema);
+mongoose.model("Instalacao", instalacaoSchema);
+mongoose.model("IPPool", ipPoolSchema);
+mongoose.model("Log", logSchema);
+mongoose.model("OLT", oltSchema);
+mongoose.model("PerfilUsuario", perfilUsuarioSchema);
+mongoose.model("Plano", planoSchema);
+mongoose.model("Usuario", usuarioSchema);
 
 export = mongoose;
