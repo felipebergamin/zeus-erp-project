e6c067c689da21e85b47171f6f820ee006262ed9
Felipe Bergamin <felipebergamin6@gmail.com>
2018-08-07 15:45:06 -0300

    corrige identacao

--- packages/server/src/graphql/resources/fiberhome/fiberhome.resolver.ts
+++ packages/server/src/graphql/resources/fiberhome/fiberhome.resolver.ts
@@ -8,19 +8,19 @@ import { FiberhomeService } from '../../../services/FiberhomeService';
 export const fiberhomeResolvers = {
 
   Query: {
-    consultarSinalONUPA: compose(...authResolvers)(async (parent, {pontoAcesso}, context: ResolverContext, info) => {
+    consultarSinalONUPA: compose(...authResolvers)(async (parent, { pontoAcesso }, context: ResolverContext, info) => {
       try {
 
         const paInstance = await context.db.PontoAcesso.findById(pontoAcesso);
-        
+
         throwError(!paInstance, `Cliente com id ${pontoAcesso} não encontrado`);
-        
+
         const olt = await context.db.OLT.findById(paInstance.get('olt'));
         throwError(!olt, `O Cliente não está associado a nenhuma OLT!`);
-        
+
         const sinal = await (await FiberhomeService.instance())
-        .getOnuOpticalInfo(olt.get('ip'), paInstance.get('slotNo'), paInstance.get('ponNo'), paInstance.get('macOnu'));
-        
+          .getOnuOpticalInfo(olt.get('ip'), paInstance.get('slotNo'), paInstance.get('ponNo'), paInstance.get('macOnu'));
+
         return sinal.parsedResponse.values[0];
       } catch (err) {
         throw new Error(`${err.name}: ${err.message}`);
@@ -28,7 +28,7 @@ export const fiberhomeResolvers = {
     }),
 
     // consultarSinalONU(olt: Int!, slot: Int!, pon: Int!, onuMac: String!): SinalONU
-    consultarSinalONU: compose(...authResolvers)(async (parent, {olt, slot, pon, onuMac}, context: ResolverContext, info) => {
+    consultarSinalONU: compose(...authResolvers)(async (parent, { olt, slot, pon, onuMac }, context: ResolverContext, info) => {
       const oltInstance = await context.db.OLT.findById(olt);
       throwError(!oltInstance, `OLT ${olt} não encontrada!`);
 
