54144da96afa80b41a2c098f7094a53afb7dd2ee
Felipe Bergamin <felipebergamin6@gmail.com>
2018-09-06 15:06:50 -0300

    resolver para ponto de acesso
    implementa resolver que busca ponto de acesso pelo id
    o id pode estar na rota ou na querystring

---
+++ packages/www/src/app/core/services/ponto-acesso/ponto-acesso-by-id-resolver.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { Observable, of } from 'rxjs';
+
+import { PontoAcesso } from '../../models/PontoAcesso';
+import { PontoAcessoService } from './ponto-acesso.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PontoAcessoByIdResolver implements Resolve<PontoAcesso> {
+  private routeparamname = 'idpontoacesso';
+
+  constructor(private _service: PontoAcessoService) { }
+
+  resolve(route: ActivatedRouteSnapshot): Observable<PontoAcesso> {
+    const id = parseInt(route.paramMap.has(this.routeparamname) ?
+      route.paramMap.get(this.routeparamname) :
+      route.queryParamMap.get(this.routeparamname), 10);
+
+    return id ? this._service.getByID(id) : null;
+  }
+}
