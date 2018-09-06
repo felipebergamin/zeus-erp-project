8ad27a9432d0e9b85ca224d8e541d8e0d66fcf62
Felipe Bergamin <felipebergamin6@gmail.com>
2018-09-06 16:09:57 -0300

    implementa query listarInstalacoes

--- packages/www/src/app/core/services/instalacao/instalacao.graphql.ts
+++ packages/www/src/app/core/services/instalacao/instalacao.graphql.ts
@@ -1,4 +1,5 @@
 import gql from 'graphql-tag';
+import { Instalacao } from '../../models/Instalacao';
 
 export interface AbrirInstalacaoInput {
   observacoesAtendente?: string;
@@ -11,11 +12,47 @@ export interface AbrirInstalacaoInput {
   tecnicoResponsavel?: number;
 }
 
+export interface ListarInstalacoesQuery {
+  listarInstalacoes: Instalacao[];
+  totalInstalacoes: number;
+}
+
 export const CREATE_INSTALACAO_MUTATION = gql`
   mutation abrirInstalacao($input: AbrirInstalacaoInput!) {
     abrirInstalacao(input: $input) {
       _id
       protocolo
+    }
+  }
+`;
+
+export const LISTAR_INSTALACOES_QUERY = gql`
+  query listarInstalacoes($first: Int, $offset: Int) {
+    totalInstalacoes
+    listarInstalacoes(first: $first, offset: $offset) {
+      _id
+      dataAgenda
+      protocolo
+      atendente {
+        _id
+        nome
+      }
+      tecnicoResponsavel {
+        _id
+        nome
+      }
+      pontoAcesso {
+        _id
+        login
+        logradouro
+        numero
+        cidade
+
+        cliente {
+          _id
+          nome
+        }
+      }
     }
   }
 `;
--- packages/www/src/app/core/services/instalacao/instalacao.service.ts
+++ packages/www/src/app/core/services/instalacao/instalacao.service.ts
@@ -3,7 +3,7 @@ import { Apollo } from 'apollo-angular';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { AbrirInstalacaoInput, CREATE_INSTALACAO_MUTATION } from './instalacao.graphql';
+import { AbrirInstalacaoInput, CREATE_INSTALACAO_MUTATION, LISTAR_INSTALACOES_QUERY, ListarInstalacoesQuery } from './instalacao.graphql';
 import { Instalacao } from '../../models/Instalacao';
 
 @Injectable({
@@ -18,5 +18,14 @@ export class InstalacaoService {
       mutation: CREATE_INSTALACAO_MUTATION,
       variables: { input },
     }).pipe(map(res => res.data.abrirInstalacao));
+  }
+
+  listarInstalacoes(variables: {first?: number, offset?: number} = {}): Observable<ListarInstalacoesQuery> {
+    return this.apollo.query<ListarInstalacoesQuery>({
+      query: LISTAR_INSTALACOES_QUERY,
+      variables,
+    }).pipe(
+      map(res => res.data)
+    );
   }
 }
