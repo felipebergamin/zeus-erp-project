c0af7eedd03066620690a6007c011ac84fd21b1a
Felipe Bergamin <felipebergamin6@gmail.com>
2018-09-12 16:52:19 -0300

    pesquisa de clientes na listagem de clientes
    implementa o recurso de pesquisa de clientes
    no component de listagem

--- packages/www/src/app/cliente/cliente/listar-clientes/listar-clientes.component.ts
+++ packages/www/src/app/cliente/cliente/listar-clientes/listar-clientes.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { PageEvent, MatBottomSheet } from '@angular/material';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PageEvent, MatBottomSheet, MatDialog, MatPaginator } from '@angular/material';
 import { ReplaySubject } from 'rxjs';
 
 import { ClienteService } from '../../../core/services/cliente/cliente.service';
 import { Cliente } from '../../../core/models/Cliente';
 import { ClienteActionSheetComponent } from '../cliente-action-sheet/cliente-action-sheet.component';
+import { FormPesquisaClienteComponent } from '../form-pesquisa-cliente/form-pesquisa-cliente.component';
 
 @Component({
   selector: 'app-listar-clientes',
@@ -17,6 +18,8 @@ export class ListarClientesComponent implements OnInit {
   pageSizeOptions = [25, 50, 75, 100];
   totalItensPaginator = 0;
   pageEvent: PageEvent;
+  searching = false;
+  searchTerms: any;
 
   /* data table */
   _dataSource = new ReplaySubject<Cliente[]>(1);
@@ -24,6 +27,7 @@ export class ListarClientesComponent implements OnInit {
   constructor(
     private bottomSheet: MatBottomSheet,
     private clienteService: ClienteService,
+    private dialog: MatDialog,
   ) { }
 
   get dataSource() {
@@ -41,6 +45,7 @@ export class ListarClientesComponent implements OnInit {
   }
 
   onPaginationChange(event: PageEvent) {
+    this.pageEvent = event;
     this.clienteService.simpleList({ first: event.pageSize, offset: event.pageIndex * event.pageSize, excluded: false })
       .subscribe(res => {
         this._dataSource.next(res.listCustomers);
@@ -52,6 +57,47 @@ export class ListarClientesComponent implements OnInit {
     this.bottomSheet.open(ClienteActionSheetComponent, {
       data: cli
     });
+  }
+
+  openSearchDialog() {
+    this.dialog.open(FormPesquisaClienteComponent, {
+      data: { previousSearchValues: this.searchTerms }
+    })
+      .afterClosed()
+      .subscribe(data => {
+        if (!data) {
+          return;
+        }
+
+        this.searchTerms = data;
+
+        this.clienteService.search(data)
+          .subscribe(result => {
+            this.searching = true;
+            this.totalItensPaginator = result.length;
+            this._dataSource.next(result);
+          });
+      });
+  }
+
+  clearSearch() {
+    this.searching = this.searchTerms = false;
+    let first;
+    let offset;
+
+    if (this.pageEvent) {
+      first = this.pageEvent.pageSize;
+      offset = this.pageEvent.pageIndex * this.pageEvent.pageSize;
+    } else {
+      first = this.pageSizeOptions[0];
+      offset = 0;
+    }
+
+    this.clienteService.simpleList({ first, offset, excluded: false })
+      .subscribe(res => {
+        this._dataSource.next(res.listCustomers);
+        this.totalItensPaginator = res.totalCustomers;
+      });
   }
 
 }
