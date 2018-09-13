810f935e0776a1102def8d797aa39b62bd5cc948
Felipe Bergamin <felipebergamin6@gmail.com>
2018-09-13 14:51:16 -0300

    refat: CpfCnpjAlreadyExists
    remove codigo debounce que nao estava sendo usado
    no validator CpfCnpjAlreadyExists

--- packages/www/src/app/form-validators/cpf-cnpj-already-exists.ts
+++ packages/www/src/app/form-validators/cpf-cnpj-already-exists.ts
@@ -3,24 +3,12 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { ClienteService } from '../core/services/cliente/cliente.service';
-let timeout;
 
 export function CpfCnpjAlreadyExists(clienteService: ClienteService): AsyncValidatorFn {
   return (control: AbstractControl): Observable<ValidationErrors | null> => {
-    if (timeout) {
-      console.log('limpando timeout');
-      clearTimeout(timeout);
-    }
-    timeout = setTimeout(() => console.log('timeout'), 2000);
-    console.log('async validator');
     const cpfCnpj = control.value;
 
     return clienteService.cpfCnpjAlreadyExists(cpfCnpj)
-      .pipe(
-        map(res => {
-          console.log(res);
-          return res ? { alreadyExists: true } : null;
-        })
-      );
+      .pipe(map(res => res ? { alreadyExists: true } : null));
   };
 }
