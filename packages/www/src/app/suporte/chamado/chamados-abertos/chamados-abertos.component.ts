7f8be7c830cc731a7e263ff4de944e618dfe53f0
Felipe Bergamin <felipebergamin6@gmail.com>
2018-09-24 14:05:19 -0300

    implementa chamado-action-sheet na listagem de chamados

--- packages/www/src/app/suporte/chamado/chamados-abertos/chamados-abertos.component.ts
+++ packages/www/src/app/suporte/chamado/chamados-abertos/chamados-abertos.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { PageEvent, MatBottomSheet } from '@angular/material';
+import { PageEvent, MatSnackBar } from '@angular/material';
 import { Subject } from 'rxjs';
 import { tap, map } from 'rxjs/operators';
 
 import { Chamado } from '../../../core/models/Chamado';
 import { ChamadoService } from '../../../core/services/chamado/chamado.service';
-import { ChamadoActionSheetComponent } from '../chamado-action-sheet/chamado-action-sheet.component';
 
 @Component({
   selector: 'app-chamados-abertos',
@@ -16,11 +15,12 @@ export class ChamadosAbertosComponent implements OnInit {
   displayedColumns = ['menu', 'protocolo', 'paLogin', 'abertoPor', 'abertoEm', 'motivoAbertura', 'prioridade'];
   totalItensPaginator = 0;
   pageSizeOptions = [10, 25, 50];
+  lastPageEvent: PageEvent;
   dataSource = new Subject<Chamado[]>();
 
   constructor(
     private chamadoService: ChamadoService,
-    private bottomSheet: MatBottomSheet,
+    private snackbar: MatSnackBar,
   ) { }
 
   ngOnInit() {
@@ -29,13 +29,18 @@ export class ChamadosAbertosComponent implements OnInit {
   }
 
   onPaginationChange(event: PageEvent) {
+    this.lastPageEvent = event;
+
     const first = event.pageSize;
     const offset = event.pageSize * event.pageIndex;
 
     this.refreshTable({ first, offset });
   }
 
-  refreshTable({first = 10, offset = 0} = {}) {
+  refreshTable({ first, offset }: { first?: number, offset?: number} = {}) {
+    first = first || (this.lastPageEvent ? this.lastPageEvent.pageSize : this.pageSizeOptions[0]);
+    offset = offset || (this.lastPageEvent ? this.lastPageEvent.pageSize * this.lastPageEvent.pageIndex : 0);
+
     this.chamadoService.chamadosAbertos({ first, offset })
       .pipe(
         tap(res => this.totalItensPaginator = res.totalChamadosAbertos),
@@ -43,8 +48,9 @@ export class ChamadosAbertosComponent implements OnInit {
       ).subscribe(this.dataSource);
   }
 
-  showActionSheet(chamado: Chamado) {
-    this.bottomSheet.open(ChamadoActionSheetComponent, { data: chamado });
+  chamadoCancelado(chamado: Chamado) {
+    this.snackbar.open('Chamado cancelado', 'Ok', { duration: 4000 });
+    this.refreshTable();
   }
 
 }
